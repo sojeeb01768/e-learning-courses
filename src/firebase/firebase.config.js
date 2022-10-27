1e61a871a6e99c7cd2f85e794fdec718ee35a42b
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-10-27 19:14:37 +0600

    added precess env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8VgWspJ1JMTDLyhvQ2tWguvWu8OcKm4k",
-  authDomain: "assignment-10-e-learning.firebaseapp.com",
-  projectId: "assignment-10-e-learning",
-  storageBucket: "assignment-10-e-learning.appspot.com",
-  messagingSenderId: "988770319733",
-  appId: "1:988770319733:web:af4fc6078e72c2d779a73c"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
