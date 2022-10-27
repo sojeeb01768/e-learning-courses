0775ecd2aaf09bb7501356428b53ab46265bef73
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-10-27 07:42:52 +0600

    added blog

--- src/components/Pages/Blog/Blog.js
+++ src/components/Pages/Blog/Blog.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>This is blog</h2>
+
+        <div className='my-5'>
+            <div className='border m-2 p-2 rounded'>
+                <h4>What is cors</h4>
+                <p>Answer: CORS stands for Cross-Origin Resource Sharing. It allows us to relax the security applied to an API. This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API. <br />
+                    In other words, CORS is a browser security feature that restricts cross-origin HTTP requests with other servers and specifies which domains access your resources.</p>
+            </div>
+
+
+            <div className='border m-2 p-2 rounded'>
+                <h4>Why are you using firebase? What other options do you have to implement authentication?</h4>
+                <p>Answer: Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more. <br />
+                    Firebase Authentication integrates tightly with other Firebase services, and it leverages industry standards like OAuth 2.0 and OpenID Connect, so it can be easily integrated with your custom backend. <br />
+                    When you upgrade to Firebase Authentication with Identity Platform, you unlock additional features, such as multi-factor authentication, blocking functions, user activity and audit logging, SAML and generic OpenID Connect support, multi-tenancy, and enterprise-level support.</p>
+            </div>
+
+
+            <div className='border m-2 p-2 rounded'>
+                <h4>How does the private route work?</h4>
+                <p>The react private route component renders child components (children) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property. <br />
+
+                    The current logged in user (authUser) is retrieved from Redux state with a call to the useSelector() hook. Redux is used in this example however it is not required to implement a Private Route component in React Router 6. You could use a different state management library or any approach you prefer to get the logged in status of the user.</p>
+            </div>
+
+
+
+            <div className='border m-2 p-2 rounded'>
+                <h4>What is cors</h4>
+                <p>Node.js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node.js wastes no time or resources on waiting for I/O requests to return. <br />
+                    In the traditional approach to creating web servers, for each incoming request or connection the server spawns a new thread of execution or even forks a new process to handle the request and send a response. Conceptually, this makes perfect sense, but in practice it incurs a great deal of overhead.
+
+                    While spawning threads incurs less memory and CPU overhead than forking processes, it can still be inefficient. The presence of a large number of threads can cause a heavily loaded system to spend precious cycles on thread scheduling and context switching, which adds latency and imposes limits on scalability and throughput.</p>
+            </div>
         </div>
+
+
+
     );
 };
 
